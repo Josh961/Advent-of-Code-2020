149e5321906f1a4960bdbf7db7f8a906308a1c3c
Joshua Zinkovsky <joshua.zky@gmail.com>
2020-12-02 01:24:54 -0500

    Add utils file for common operations

---
+++ utils.ts
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+export class Utils {
+
+  public static parseNumbersFromFile = (filePath: string): number[] => {
+    const numbers: any[] = fs.readFileSync(filePath, 'utf8').split(/[\r\n]+/);
+    for (let i = 0; i < numbers.length; i++) {
+      numbers[i] = +numbers[i];
+    }
+
+    return numbers;
+  }
+}
