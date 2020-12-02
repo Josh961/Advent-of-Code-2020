0629fa527dc5f850cb14412519c3085ff9de367a
Joshua Zinkovsky <joshua.zky@gmail.com>
2020-12-02 01:25:11 -0500

    Day 1: Report Repair

---
+++ 01.ts
@@ -0,0 +1,20 @@
+// Day 1: Report Repair
+// https://adventofcode.com/2020/day/1
+
+import { Utils } from './utils';
+
+const numbers = Utils.parseNumbersFromFile('./inputs/01.txt');
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = i + 1; j < numbers.length; j++) {
+    if (numbers[i] + numbers[j] === 2020) {
+      console.log(numbers[i] * numbers[j]);
+    }
+
+    for (let k = j + 1; k < numbers.length; k++) {
+      if (numbers[i] + numbers[j] + numbers[k] === 2020) {
+        console.log(numbers[i] * numbers[j] * numbers[k]);
+      }
+    }
+  }
+}
