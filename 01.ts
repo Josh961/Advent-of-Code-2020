55b7cc0e4faf41778ff1d4c976cf850ed07b8ed4
Josh <joshua.zky@gmail.com>
2020-12-04 19:46:15 -0500

    Optimize final loop to run only if previous two numbers are less than 2020

--- 01.ts
+++ 01.ts
@@ -9,11 +9,11 @@ for (let i = 0; i < numbers.length; i++) {
   for (let j = i + 1; j < numbers.length; j++) {
     if (numbers[i] + numbers[j] === 2020) {
       console.log(numbers[i] * numbers[j]);
-    }
-
-    for (let k = j + 1; k < numbers.length; k++) {
-      if (numbers[i] + numbers[j] + numbers[k] === 2020) {
-        console.log(numbers[i] * numbers[j] * numbers[k]);
+    } else if (numbers[i] + numbers[j] < 2020) {
+      for (let k = j + 1; k < numbers.length; k++) {
+        if (numbers[i] + numbers[j] + numbers[k] === 2020) {
+          console.log(numbers[i] * numbers[j] * numbers[k]);
+        }
       }
     }
   }
