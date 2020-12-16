9ea256ded812e4305b098c047c5470b02a179f9f
Josh <joshua.zky@gmail.com>
2020-12-15 23:12:22 -0500

    Day 9: Encoding Error (part two)

--- 09.ts
+++ 09.ts
@@ -5,8 +5,10 @@ import { Utils } from './utils';
 
 const numbers = Utils.parseNumbersFromFile('./inputs/09.txt');
 
-const summableNumbers = numbers.splice(0, 25);
-for (const number of numbers) {
+let invalidNumber: number;
+const numbersCopy = [...numbers];
+const summableNumbers = numbersCopy.splice(0, 25);
+for (const number of numbersCopy) {
   let sumsToNumber = false;
   for (let i = 0; i < summableNumbers.length; i++) {
     for (let j = 0; j < summableNumbers.length; j++) {
@@ -21,7 +23,39 @@ for (const number of numbers) {
   }
 
   if (!sumsToNumber) {
-    console.log(number);
+    invalidNumber = number;
     break;
   }
 }
+
+console.log(invalidNumber);
+
+let sumOfMinMax: number;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] >= invalidNumber) { continue; }
+
+  let sumsToInvalidNumber = false;
+  let currentSum = numbers[i];
+  const contiguousNumbers = [numbers[i]];
+  for (let j = i + 1; j < numbers.length; j++) {
+    if (currentSum + numbers[j] < invalidNumber) {
+      currentSum += numbers[j];
+      contiguousNumbers.push(numbers[j]);
+    } else if (currentSum + numbers[j] > invalidNumber) {
+      break;
+    } else {
+      contiguousNumbers.push(numbers[j]);
+      sumsToInvalidNumber = true;
+      break;
+    }
+  }
+
+  if (sumsToInvalidNumber) {
+    const min = Math.min(...contiguousNumbers);
+    const max = Math.max(...contiguousNumbers);
+    sumOfMinMax = min + max;
+    break;
+  }
+}
+
+console.log(sumOfMinMax);
