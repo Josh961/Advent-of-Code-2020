1fa61b1e1b012dca88c254e2e677eb98a20d38f8
Josh <joshua.zky@gmail.com>
2021-01-23 17:14:23 -0500

    Day 14: Docking Data (part two)

--- 14.ts
+++ 14.ts
@@ -5,7 +5,7 @@ import { Utils } from './utils';
 
 const lines = Utils.parseFile('./inputs/14.txt');
 
-const mem: Record<string, number> = {};
+let mem: Record<string, number> = {};
 let mask;
 for (const line of lines) {
   const splitLine = line.split(' = ');
@@ -16,8 +16,33 @@ for (const line of lines) {
     mask = rightHand;
   } else {
     const value = calculateValueWithMask(+rightHand, mask);
-    const memoryAddress = leftHand.substring(4, leftHand.length - 1);
-    mem[memoryAddress] = value;
+    const address = leftHand.substring(4, leftHand.length - 1);
+    mem[address] = value;
+  }
+}
+
+console.log(Object.values(mem).reduce((a, b) => a + b));
+
+mem = {};
+for (const line of lines) {
+  const splitLine = line.split(' = ');
+  const leftHand = splitLine[0];
+  const rightHand = splitLine[1];
+
+  if (leftHand === 'mask') {
+    mask = rightHand;
+  } else {
+    const value = +rightHand;
+    const address = +leftHand.substring(4, leftHand.length - 1);
+    const binary = address.toString(2).padStart(mask.length, '0');
+    const binaryArray = [...binary];
+
+    for (let i = 0; i < binaryArray.length; i++) {
+      if (mask[i] === 'X') { binaryArray[i] = 'X'; }
+      else if (mask[i] === '1') { binaryArray[i] = '1'; }
+    }
+
+    dfs(binaryArray, 0, value, mem);
   }
 }
 
@@ -28,10 +53,38 @@ function calculateValueWithMask(num: number, mask: string): number {
   const binaryArray = [...binary];
 
   for (let i = 0; i < binaryArray.length; i++) {
-    if (mask[i] === 'X') { continue; }
-    else if (mask[i] === '0') { binaryArray[i] = '0'; }
-    else { binaryArray[i] ='1'; }
+    if (mask[i] === '0') { binaryArray[i] = '0'; }
+    else if (mask[i] === '1') { binaryArray[i] = '1'; }
   }
 
   return parseInt(binaryArray.join(''), 2);
+}
+
+function dfs(binary: string[], index: number, value: number, mem: Record<string, number>): void {
+  if (index === 35) {
+    if (binary[index] === 'X') {
+      binary[index] = '0';
+      let address = parseInt(binary.join(''), 2);
+      mem[address] = value;
+      binary[index] = '1';
+      address = parseInt(binary.join(''), 2);
+      mem[address] = value;
+    } else {
+      const address = parseInt(binary.join(''), 2);
+      mem[address] = value;
+    }
+    return;
+  }
+
+  if (binary[index] === 'X') {
+    const nextIndex = index;
+    const binaryCopy = [...binary];
+
+    binary[index] = '0';
+    dfs(binary, nextIndex, value, mem);
+    binaryCopy[index] = '1';
+    dfs(binaryCopy, nextIndex, value, mem);
+  } else {
+    dfs(binary, ++index, value, mem);
+  }
 }
